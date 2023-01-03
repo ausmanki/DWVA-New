c71304c6dc67e0b8df7a1dcacb6ecbe2bf252e50
ausmanki <100470886+ausmanki@users.noreply.github.com>
2023-01-03 19:32:56 +0530

    Update StartupListener.java

--- AltoroJ-AltoroJ-3.2/src/com/ibm/security/appscan/altoromutual/listener/StartupListener.java
+++ AltoroJ-AltoroJ-3.2/src/com/ibm/security/appscan/altoromutual/listener/StartupListener.java
@@ -24,7 +24,7 @@ public class StartupListener implements ServletContextListener {
 	}
 
 	@Override
-	public void contextDestroyed(ServletContextEvent sce) {
+	public void contextDestroyed(ServletContextEvent sce) { 
 		// do nothing
 	}
 
